b2139a0bf474fa65949a1b9cb45cfa761735c8cf
carbon2204 <dmitry.kuzmin.work@gmail.com>
2024-09-13 13:29:07 +0300

    Initial commit
    Signed-off-by: carbon2204 <dmitry.kuzmin.work@gmail.com>

---
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.example.musicapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.musicapp.adapter.CategoryAdapter
+import com.example.musicapp.adapter.SectionSongListAdapter
+import com.example.musicapp.databinding.ActivityMainBinding
+import com.example.musicapp.models.CategoryModel
+import com.example.musicapp.models.SongModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.toObjects
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var categoryAdapter: CategoryAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+        getCategories()
+        setupSection("section_1",binding.section1MainLayout,binding.section1Title,binding.section1RecyclerView)
+        setupSection("section_2",binding.section2MainLayout,binding.section2Title,binding.section2RecyclerView)
+        setupSection("section_3",binding.section3MainLayout,binding.section3Title,binding.section3RecyclerView)
+        setupMostlyPlayed("mostly_played",binding.mostlyPlayedMainLayout,binding.mostlyPlayedTitle,binding.mostlyPlayedRecyclerView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showPlayerView()
+    }
+
+    fun showPlayerView(){
+        binding.playerView.setOnClickListener {
+            startActivity(Intent(this,PlayerActivity::class.java))
+        }
+        MyExoplayer.getCurrentSong()?.let {
+            binding.playerView.visibility = View.VISIBLE
+            binding.songTitleTextView.text = "Now Playing : " + it.title
+            Glide.with(binding.songCoverImageView).load(it.coverUrl)
+                .apply(
+                    RequestOptions().transform(RoundedCorners(32))
+                ).into(binding.songCoverImageView)
+        } ?: run{
+            binding.playerView.visibility = View.GONE
+        }
+    }
+
+    //Category
+
+
+    fun getCategories(){
+        FirebaseFirestore.getInstance().collection("category")
+            .get().addOnSuccessListener {
+                val categoryList = it.toObjects(CategoryModel::class.java)
+                setupCategoryRecyclerView(categoryList)
+            }
+    }
+
+    fun setupCategoryRecyclerView(categoryList : List<CategoryModel>){
+        categoryAdapter = CategoryAdapter(categoryList)
+        binding.categoriesRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        binding.categoriesRecyclerView.adapter = categoryAdapter
+    }
+
+    //Sections
+    fun setupSection(id : String, mainLayout : RelativeLayout, titleView : TextView, recyclerView: RecyclerView){
+        FirebaseFirestore.getInstance().collection("sections")
+            .document(id)
+            .get().addOnSuccessListener {
+                val section = it.toObject(CategoryModel::class.java)
+                section?.apply {
+                    mainLayout.visibility = View.VISIBLE
+                    titleView.text = name
+                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+                    recyclerView.adapter = SectionSongListAdapter(songs)
+                    mainLayout.setOnClickListener {
+                        SongsListActivity.category = section
+                        startActivity(Intent(this@MainActivity,SongsListActivity::class.java))
+                    }
+                }
+            }
+
+    }
+
+    //Sections
+    fun setupMostlyPlayed(id : String, mainLayout : RelativeLayout, titleView : TextView, recyclerView: RecyclerView){
+        FirebaseFirestore.getInstance().collection("sections")
+            .document(id)
+            .get().addOnSuccessListener {
+                //most played songs
+                FirebaseFirestore.getInstance().collection("songs")
+                    .orderBy("count", Query.Direction.DESCENDING)
+                    .limit(5)
+                    .get().addOnSuccessListener {songListSnapshot->
+                       val songsModelList =  songListSnapshot.toObjects<SongModel>()
+                       val songsIdList = songsModelList.map {
+                           it.id
+                       }.toList()
+                        val section = it.toObject(CategoryModel::class.java)
+                        section?.apply {
+                            mainLayout.visibility = View.VISIBLE
+                            titleView.text = name
+                            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+                            recyclerView.adapter = SectionSongListAdapter(songsIdList)
+                            mainLayout.setOnClickListener {
+                                SongsListActivity.category = section
+                                startActivity(Intent(this@MainActivity,SongsListActivity::class.java))
+                            }
+                        }
+                    }
+
+            }
+
+    }
+
+
+
+}
